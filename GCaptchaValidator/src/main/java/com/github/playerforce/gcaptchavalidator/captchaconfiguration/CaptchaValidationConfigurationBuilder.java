1706dad3e806b813ce8ff4d534238b8782f19312
PlayerForceHD <P.Zarrad@web.de>
2016-12-22 19:09:53 +0100

    Initial commit

---
+++ GCaptchaValidator/src/main/java/com/github/playerforce/gcaptchavalidator/captchaconfiguration/CaptchaValidationConfigurationBuilder.java
@@ -0,0 +1,88 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016 PlayerForceHD
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.playerforce.gcaptchavalidator.captchaconfiguration;
+
+/**
+ * A builder for easy creation of the {@link CaptchaValidationConfiguration}
+ *
+ * @author PlayerForceHD
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class CaptchaValidationConfigurationBuilder {
+
+
+    /**
+     * The configuration that is build with the current builder instance
+     */
+    private CaptchaValidationConfiguration captchaValidationConfiguration;
+
+    public CaptchaValidationConfigurationBuilder() {
+        this.captchaValidationConfiguration = new CaptchaValidationConfiguration();
+    }
+
+    /**
+     * Set the shared key between your site and ReCaptcha.
+     *
+     * @param secret The shared key between your site and ReCaptcha.
+     * @return The instance of this builder
+     */
+    public CaptchaValidationConfigurationBuilder withSecret(String secret) {
+        this.captchaValidationConfiguration.setSecret(secret);
+        return this;
+    }
+
+    /**
+     * Set the user response token provided by ReCaptcha, verifying the user on your site.
+     *
+     * @param response The user response token provided by ReCaptcha, verifying the user on your site.
+     * @return The instance of this builder
+     */
+    public CaptchaValidationConfigurationBuilder withResponse(String response) {
+        this.captchaValidationConfiguration.setResponse(response);
+        return this;
+    }
+
+    /**
+     * Optional: Set user's IP address.
+     *
+     * @param remoteIP The user's IP address.
+     * @return The instance of this builder
+     */
+    public CaptchaValidationConfigurationBuilder withRemoteIP(String remoteIP) {
+        this.captchaValidationConfiguration.setRemoteip(remoteIP);
+        return this;
+    }
+
+    /**
+     * Returns the built configuration
+     *
+     * @return The configuration built with this builder
+     */
+    public CaptchaValidationConfiguration build() {
+        return captchaValidationConfiguration;
+    }
+
+}
